a15be6d819c4b5a906990d8cef1f3c718abfd609
Davide Cossu <cossu.dvd@gmail.com>
2021-07-06 10:12:54 +0200

    Add faction icon

--- make_leaders.py
+++ make_leaders.py
@@ -18,7 +18,6 @@ import os
 def main():
     
     directories = [x[1] for x in os.walk("./source/")][0]
-    print(directories)
 
     with open("leader.tex") as f:
         card_file_content = f.read().splitlines()
@@ -27,30 +26,35 @@ def main():
         pieces = faction.split("-")
         fname = pieces[0]
         fcolor = pieces[1]
+        icon = ""
+        for leader in os.scandir("./source/"+faction):
+            if "icon" in leader.name:
+                icon = "./"+leader.name
         for leader in os.scandir("./source/"+faction):
             if not leader.name.endswith(".tex"):
-                chunks = leader.name.split("-")
-                print(chunks)
-                name = chunks[0].replace(".png","").replace("_"," ")
-                strength = chunks[1].replace(".png","").replace(".jpg","")
-                art = "./"+leader.name
-                print(name, strength,art)
+                if not "icon" in leader.name:
+                    chunks = leader.name.split("-")
+                    name = chunks[0].replace(".png","").replace("_"," ")
+                    strength = chunks[1].replace(".png","").replace(".jpg","")
+                    art = "./"+leader.name
 
-                fo = open("./source/"+faction+"/"+name.replace(" ","_")+".tex","w+")
-            
-                for line in card_file_content:
-                    if "\\newcommand\\leaderName" in line:
-                        line = line.replace("Leader-Name", name)
-                    elif "\\newcommand\\leaderStrength" in line:
-                        line = line.replace("1", strength)
-                    elif "\\newcommand\\leaderArt" in line:
-                        line = line.replace("example-image", art)
-                    elif "\\definecolor" in line:
-                        line = line.replace("000,111,222", fcolor)
+                    fo = open("./source/"+faction+"/"+name.replace(" ","_")+".tex","w+")
+                
+                    for line in card_file_content:
+                        if "\\newcommand\\leaderName" in line:
+                            line = line.replace("Leader-Name", name)
+                        elif "\\newcommand\\leaderStrength" in line:
+                            line = line.replace("1", strength)
+                        elif "\\newcommand\\leaderArt" in line:
+                            line = line.replace("example-image", art)
+                        elif "\\newcommand\\factionArt" in line:
+                            line = line.replace("example-image", icon)
+                        elif "\\definecolor" in line:
+                            line = line.replace("000,111,222", fcolor)
 
-                    fo.write(line+"\n")
+                        fo.write(line+"\n")
 
-                fo.close()
+                    fo.close()
 
 
 if __name__ == "__main__":
